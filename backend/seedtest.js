9ee305a5c705b34cbf8ddb9e851739b61db86c26
Ayush nema <nemaayush22@gmail.com>
2026-05-14 12:41:22 +0530

    remove duplicate data

--- backend/seedtest.js
+++ backend/seedtest.js
@@ -21,14 +21,31 @@ mongoose
         strict: true,
       }),
     }));
+    const names = new Set();
 
-    await MasterTest.insertMany(updatedTests);
+data.tests.forEach((test) => {
+  if (names.has(test.name)) {
+    console.log(test.name);
+  }
+  names.add(test.name);
+});
+
+    const uniqueTests = Array.from(
+  new Map(
+    updatedTests.map((item) => [
+      item.name,
+      item,
+    ])
+  ).values()
+);
+
+await MasterTest.insertMany(uniqueTests);
 
     console.log("Master Tests Inserted Successfully");
 
     process.exit();
   })
   .catch((err) => {
-    console.log(err);
+    console.log(err.message);
     process.exit(1);
   });
